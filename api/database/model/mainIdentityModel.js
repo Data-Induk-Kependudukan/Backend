c0c216f486c3e2f38912fe04f4cab5b9c82dec32
AlexSinagaa <80649417+AlexSinagaa@users.noreply.github.com>
2021-07-31 20:08:58 +0700

    second crud updating

--- api/database/model/mainIdentityModel.js
+++ api/database/model/mainIdentityModel.js
@@ -1,26 +1,26 @@
 const mainIdentityModelInsert = (data) => {
     return {
-        nik: data.nik,
-        namalengkap: data.namalengkap,
-        tempatlahir: data.tempatlahir,
-        tanggallahir: { date: data.tanggallahir },
-        jeniskelamin: data.jeniskelamin,
-        golongandarah: data.golongandarah,
+        NIK: data.NIK,
+        nama_lkp: data.nama_lkp,
+        tmp_lhr: data.tmp_lhr,
+        tgl_lahir: data.tgl_lahir,
+        jns_klmn: data.jns_klmn,
+        goldar: data.goldar,
         alamat: {
-            alamat: data.alamat,
-            rt: data.rt,
-            rw: data.rw,
-            kelurahan: data.kelurahan,
-            kecamatan: data.kecamatan,
-            kota: data.kota,
-            provinsi: data.provinsi,
-            kodepos: data.kodepos
+            alamat: data.alamat.Alamat,
+            RT: data.alamat.RT,
+            rw: data.alamat.RW,
+            'Kel/Desa': data.alamat['Kel/Desa'],
+            Kec: data.alamat.Kec,
+            KabKot: data.alamat.KabKot,
+            Prov: data.alamat.Prov,
+            Pos: data.alamat.Pos
         },
         agama: data.agama,
-        status: data.status,
+        sts_kawin: data.sts_kawin,
         pekerjaan: data.pekerjaan,
         kewarganegaraan: data.kewarganegaraan,
-        tgl_pembuatan: data.tanggalpembuatan,
+        tgl_pembuatan: data.tgl_pembuatan,
         foto: data.foto
     };
 };
